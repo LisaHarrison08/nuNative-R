222cb8ac529340bd602f51471523c8540c51d021
Lisa Harrison <lmharrison0810@gmail.com>
2022-01-26 21:02:03 -0500

    updated components to connect to redux

--- nucampsite/components/AboutComponent.js
+++ nucampsite/components/AboutComponent.js
@@ -1,16 +1,17 @@
 import React, { Component } from "react";
-import { ScrollView, Text } from "react-native";
+import { ScrollView, Text, FlatList } from "react-native";
 import { Card, ListItem } from "react-native-elements";
-import { FlatList } from "react-native-gesture-handler";
-import { PARTNERS } from "../shared/partners";
+import { connect } from 'react-redux';
+import { baseUrl } from '../shared/baseUrl';
+// import { FlatList } from "react-native-gesture-handler";
+
+const mapStateToProps = state => {
+  return {
+    partners: state.partners
+  };
+};
 
 class About extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      partners: PARTNERS,
-    };
-  }
 
   static navigationOptions = {
     title: "About Us",
@@ -22,7 +23,7 @@ class About extends Component {
         <ListItem
           title={item.name}
           subtitle={item.description}
-          leftAvatar={{ source: require("./images/bootstrap-logo.png") }}
+          leftAvatar={{ source: { uri: baseUrl + item.image } }}
         />
       );
     };
@@ -31,7 +32,7 @@ class About extends Component {
         <Mission />
         <Card title="Community Partners">
           <FlatList
-            data={this.state.partners}
+            data={this.props.partners.partners}
             renderItem={renderPartnerItem}
             keyExtractor={(item) => item.id.toString()}
           />
@@ -42,19 +43,19 @@ class About extends Component {
 }
 
 const Mission = () => {
-    return (
-      <Card title="Our Mission">
-        <Text style={{ margin: 10 }}>
-          We present a curated database of the best campsites in the vast woods
-          and backcountry of the World Wide Web Wilderness. We increase access to
-          adventure for the public while promoting safe and respectful use of
-          resources. The expert wilderness trekkers on our staff personally verify
-          each campsite to make sure that they are up to our standards. We also
-          present a platform for campers to share reviews on campsites they have
-          visited with each other.
-        </Text>
-      </Card>
-    );
-  };
+  return (
+    <Card title="Our Mission">
+      <Text style={{ margin: 10 }}>
+        We present a curated database of the best campsites in the vast woods
+        and backcountry of the World Wide Web Wilderness. We increase access to
+        adventure for the public while promoting safe and respectful use of
+        resources. The expert wilderness trekkers on our staff personally verify
+        each campsite to make sure that they are up to our standards. We also
+        present a platform for campers to share reviews on campsites they have
+        visited with each other.
+      </Text>
+    </Card>
+  );
+};
 
-export default About;
+export default connect(mapStateToProps)(About);
